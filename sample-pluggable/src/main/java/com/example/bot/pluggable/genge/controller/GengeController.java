2a198da2f55ed07c83f9a293f2a33425570eb316
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-04-18 08:42:11 +0900

    friend repository

--- sample-pluggable/src/main/java/com/example/bot/pluggable/genge/controller/GengeController.java
+++ sample-pluggable/src/main/java/com/example/bot/pluggable/genge/controller/GengeController.java
@@ -1,17 +1,23 @@
 package com.example.bot.pluggable.genge.controller;
 
+import com.example.bot.pluggable.genge.friend_list.FriendRepository;
 import com.example.bot.pluggable.genge.handler.TextContentHandler;
 import com.example.bot.pluggable.genge.handler.HandlerResponse;
+import com.linecorp.bot.client.LineBotClient;
 import com.linecorp.bot.client.exception.LineBotAPIException;
 import com.linecorp.bot.model.callback.Message;
+import com.linecorp.bot.model.content.AddedAsFriendOperation;
+import com.linecorp.bot.model.content.BlockedOperation;
 import com.linecorp.bot.model.content.Content;
 import com.linecorp.bot.model.content.TextContent;
+import com.linecorp.bot.model.profile.UserProfileResponse;
 import com.linecorp.bot.spring.boot.annotation.LineBotMessages;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -19,6 +25,10 @@ import java.util.List;
 public class GengeController {
     @Autowired
     private List<TextContentHandler> handlers;
+    @Autowired
+    private FriendRepository friendRepository;
+    @Autowired
+    private LineBotClient lineBotClient;
 
     @RequestMapping("/callback")
     public void callback(@LineBotMessages List<Message> messages) throws LineBotAPIException {
@@ -29,6 +39,18 @@ public class GengeController {
 
     private void handle(Message message) throws LineBotAPIException {
         Content content = message.getContent();
+
+        if (content instanceof AddedAsFriendOperation) {
+            String mid = ((AddedAsFriendOperation) content).getMid();
+            UserProfileResponse userProfile = lineBotClient.getUserProfile(Collections.singletonList(mid));
+            friendRepository.addedFriend(userProfile.getContacts().get(0));
+        }
+
+        if (content instanceof BlockedOperation) {
+            String mid = ((BlockedOperation) content).getMid();
+            friendRepository.blocked(mid);
+        }
+
         if (content instanceof TextContent) {
             for (TextContentHandler handler : handlers) {
                 HandlerResponse told = handler.told((TextContent) content);
--- sample-pluggable/src/main/java/com/example/bot/pluggable/genge/plugin/time_signal/TimeSignalHandler.java
+++ sample-pluggable/src/main/java/com/example/bot/pluggable/genge/plugin/time_signal/TimeSignalHandler.java
@@ -1,5 +1,6 @@
 package com.example.bot.pluggable.genge.plugin.time_signal;
 
+import com.example.bot.pluggable.genge.friend_list.FriendRepository;
 import com.linecorp.bot.client.LineBotClient;
 import com.linecorp.bot.client.exception.LineBotAPIException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,13 @@ import java.util.Collections;
 public class TimeSignalHandler {
     @Autowired
     private LineBotClient lineBotClient;
+    @Autowired
+    private FriendRepository friendRepository;
 
     @Scheduled(cron="0 * * * *")
     public void run() throws LineBotAPIException {
         // TOOD pass mid
-        lineBotClient.sendText(Collections.emptyList(),
+        lineBotClient.sendText(friendRepository.getMids(),
                 LocalDateTime.now().toString());
     }
 }
