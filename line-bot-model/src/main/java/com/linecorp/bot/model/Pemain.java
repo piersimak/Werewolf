ec810f090f11eeaae29da391cc0bc8139776a21c
PIER-PC\PIER <piersrv@gmail.com>
2017-10-15 15:58:20 +0700

    asd

---
+++ line-bot-model/src/main/java/com/linecorp/bot/model/Pemain.java
@@ -0,0 +1,12 @@
+package com.linecorp.bot.model;
+
+public class Pemain
+{
+    private String nama;
+    private String id;
+    public Pemain(String nama_, String id_)
+    {
+        this.nama = nama_;
+        this.id = id_;
+    }
+}
