1c66b4136c9f1f10a1f0bb07056ee817c43cb439
PIER-PC\PIER <piersrv@gmail.com>
2017-10-15 16:16:11 +0700

    sdfsdf

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/PemainModel.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/PemainModel.java
@@ -2,11 +2,5 @@ package com.example.bot.spring;
 
 public class PemainModel
 {
-    private String nama;
-    private String id;
-    public PemainModel(String nama_, String id_)
-    {
-        this.nama = nama_;
-        this.id = id_;
-    }
+    
 }
