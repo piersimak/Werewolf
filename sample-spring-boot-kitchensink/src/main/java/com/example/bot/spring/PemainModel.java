cbbf64e72b3b23feccacb728af0f5d78a0f89e1b
PIER-PC\PIER <piersrv@gmail.com>
2017-10-15 16:20:36 +0700

    asd

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/PemainModel.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/PemainModel.java
@@ -1,6 +1,17 @@
 package com.example.bot.spring;
 
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+/**
+ * Send messages to multiple users, groups, and rooms at any time.
+ */
+@Value
+@AllArgsConstructor
 public class PemainModel
 {
-    
+    public PemainModel()
+    {
+      
+    }
 }
