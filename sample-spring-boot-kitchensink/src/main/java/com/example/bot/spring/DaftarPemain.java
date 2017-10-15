e8ea229d33dbf9793b218ef9ae65e9ccd5e73512
PIER-PC\PIER <piersrv@gmail.com>
2017-10-15 14:25:04 +0700

    sasadasd

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DaftarPemain.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DaftarPemain.java
@@ -1,9 +1,7 @@
 package com.example.bot.spring;
 import java.util.Collections;
-import java.util.List;
 import com.linecorp.bot.model.Pemain;
 import java.util.ArrayList;
-
 public class DaftarPemain {
 
     public static List<DaftarPemain> daftarPemain = new ArrayList<DaftarPemain>();
