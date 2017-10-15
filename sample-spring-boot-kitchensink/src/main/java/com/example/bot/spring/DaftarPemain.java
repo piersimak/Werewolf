c23a11f2fe991ab850688a7e000065c7a1372cfc
PIER-PC\PIER <piersrv@gmail.com>
2017-10-15 15:03:01 +0700

    sdfsdf

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DaftarPemain.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DaftarPemain.java
@@ -2,6 +2,7 @@ package com.example.bot.spring;
 import java.util.Collections;
 import com.linecorp.bot.model.Pemain;
 import java.util.ArrayList;
+import java.util.List;
 public class DaftarPemain {
 
     public static List<DaftarPemain> daftarPemain = new ArrayList<DaftarPemain>();
